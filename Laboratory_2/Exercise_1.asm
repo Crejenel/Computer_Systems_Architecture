4ca9b5a77a92dd264c03e7d52f9fdbe2582154ab
Crejenovschi David <crejenoriginal.david@gmail.com>
2024-12-27 16:15:09 +0200

    Update Exercise_1.asm
    <<This program calculates the sum of two numbers.>> The maximum value that can be given as an operand is 2^31-1, and the minimum is -2^31. Otherwise, the numbers will not be processed correctly by the program. If the result exceeds 2^31-1, the output will not be correct, and if the result is less than -2^31, the output will also be incorrect.

--- Laboratory_2/Exercise_1.asm
+++ Laboratory_2/Exercise_1.asm
@@ -16,12 +16,6 @@ message_result db "The sum of this two numbers is : ",0
 message_write_a db "Enter the value of a : ", 0
 message_write_b db "Enter the value of b : ", 0
 
-;This program calculates the sum of two numbers. 
-;The maximum value that can be given as an operand is 2^31-1, and the minimum is -2^31. 
-;Otherwise, the numbers will not be processed correctly by the program. 
-;If the result exceeds 2^31-1, the output will not be correct,
-;and if the result is less than -2^31, the output will also be incorrect.
-
 
 segment code use32 class=code
 start:
